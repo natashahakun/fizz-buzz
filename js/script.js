d7e188de4b400f628b914f96fc01467f42de1c89
natashalynne <hakun.natasha@gmail.com>
2015-10-12 18:16:09 -0400

    Refactor fizz-buzz as a function.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,37 @@
-for (var i = 1; i <= 100; i++) {
-  if (i % 3 == 0 && i % 5 == 0) {
-    document.write(" Fizz-Buzz" + "<br />")
-  } else if (i % 3 == 0) {
-    document.write(" Fizz" + "<br />")
-  } else if (i % 5 == 0) {
-    document.write(" Buzz" + "<br />")
-  } else {
-    document.write(" " + i + "<br />")
+//fizz-buzz refactored
+
+function fizzbuzz(max) {
+  while (isNaN(max) === true || max == "" || max == " ") {
+    var max = prompt("Please enter a number");
   }
+    var num = parseInt(max);
+    for (var i = 1; i <= num; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      document.write(" Fizz-Buzz" + "<br />")
+    } else if (i % 3 == 0) {
+      document.write(" Fizz" + "<br />")
+    } else if (i % 5 == 0) {
+      document.write(" Buzz" + "<br />")
+    } else {
+      document.write(" " + i + "<br />")
+    }
+  };
 };
+
+var max = prompt("Please enter a number");
+
+fizzbuzz(max);
+
+//fizz-buzz standard
+
+// for (var i = 1; i <= 100; i++) {
+//   if (i % 3 == 0 && i % 5 == 0) {
+//     document.write(" Fizz-Buzz" + "<br />")
+//   } else if (i % 3 == 0) {
+//     document.write(" Fizz" + "<br />")
+//   } else if (i % 5 == 0) {
+//     document.write(" Buzz" + "<br />")
+//   } else {
+//     document.write(" " + i + "<br />")
+//   }
+// };
